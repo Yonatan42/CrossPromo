94bda98426f88d8a0521ad7a5df00d0435f42f56
Yonatan Giventer <yonatan.giventer@gmail.com>
2021-04-08 18:47:10 +0300

    very basic outline for core scripts

---
+++ Assets/CrossPromo/Scripts/CrossPromo/CrossPromoController.cs
@@ -0,0 +1,41 @@
+﻿namespace PromoPanel
+{
+
+    public class CrossPromoController
+    {
+
+        private readonly CrossPromoView view;
+
+        public CrossPromoController(CrossPromoView view)
+        {
+            this.view = view;
+        }
+
+        public void Next()
+        {
+
+        }
+
+        public void Pevious()
+        {
+
+        }
+
+        public void Pause()
+        {
+
+        }
+
+        public void Resume()
+        {
+
+        }
+
+
+
+        private void LoadVideoAtURL(string url)
+        {
+            view.LoadVideoAtURL(url);
+        }
+    }
+}
---
+++ Assets/CrossPromo/Scripts/CrossPromo/CrossPromoView.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+namespace PromoPanel
+{
+
+    public class CrossPromoView : MonoBehaviour
+    {
+        [SerializeField]
+        private string playerId;
+
+        [SerializeField]
+        private VideoPlayer videoPlayer;
+
+
+        public string PlayerId
+        {
+            get
+            {
+                return playerId;
+            }
+        }
+
+        public void LoadVideoAtURL(string url)
+        {
+
+        }
+    }
+}
